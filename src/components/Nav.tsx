49236632ce0a2dbea9263a24804d7442c76ff715
Hsi <its.hsichen@gmail.com>
2026-05-16 03:41:24 +0800

    perf(nav): serve responsive header logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,7 +11,16 @@ export default function Nav() {
             </a>
             <div className='nav-brand' aria-label={t.faviconAltBrand}>
                 <a className='nav-title' href='#main-content'>
-                    <img className='logo' src='images/logo.png' alt='' aria-hidden='true' />
+                    <img
+                        className='logo'
+                        src='images/logo-64.png'
+                        srcSet='images/logo-64.png 64w, images/logo-128.png 128w, images/logo.png 650w'
+                        sizes='32px'
+                        width='64'
+                        height='64'
+                        alt=''
+                        aria-hidden='true'
+                    />
                     <span className='title'>{t.faviconAltBrand}</span>
                 </a>
             </div>
