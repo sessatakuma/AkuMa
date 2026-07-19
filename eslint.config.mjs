bfd873b6f783092ca9ff99cff8ec56a47d071674
torrid-fish <justinwu6900942@gmail.com>
2026-07-20 00:25:07 +0800

    chore: exclude local build artifacts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig([
     {
-        ignores: ['.next/**', '.vercel/**', '.open-next/**', 'dist/**'],
+        ignores: ['.build/**', '.next/**', '.vercel/**', '.open-next/**', 'dist/**'],
     },
     pluginReact.configs.flat.recommended,
     js.configs.recommended,
